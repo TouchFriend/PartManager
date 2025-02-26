cf5ba1f7e4e16b2da125eaa91561f18118f6143f
TouchFriend <1604181058@qq.com>
2025-02-26 20:15:38 +0800

    日志可以输出到业务的日志系统中

--- class/Log.swift
+++ class/Log.swift
@@ -7,6 +7,14 @@
 
 import Foundation
 
+public class PartLog {
+    public static var log: ((_ message: String,
+                             _ file: String,
+                             _ line: Int,
+                             _ column: Int) -> Void)?
+}
+
+
 extension String {
     static func formateDate(_ date: Date, formate: String) -> String {
         let dateFormatter = DateFormatter()
@@ -18,12 +26,23 @@ extension String {
 func DLog(_ format: String,
           _ args: any CVarArg...,
           file: String = #file,
-          function: String = #function,
           line: Int = #line,
           column: Int = #column) {
-#if DEBUG
     let message = String(format: format, args)
+    DLog(message, file: file, line: line, column: column)
+}
+
+func DLog(_ message: String,
+          file: String = #file,
+          line: Int = #line,
+          column: Int = #column) {
+    if let log = PartLog.log {
+        log(message, file, line, column)
+        return
+    }
+#if DEBUG
     let time = String.formateDate(Date(), formate: "yyyy-MM-dd HH:mm:ss.SSSSSS")
-    print("\(time) \(file.components(separatedBy: "/").last ?? ""):\(function):\(line):\(column): \(message)")
+    print("\(time) \(file.components(separatedBy: "/").last ?? ""):\(line):\(column): \(message)")
 #endif
 }
+
